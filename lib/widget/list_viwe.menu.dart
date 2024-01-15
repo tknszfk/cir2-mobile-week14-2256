38a9047622423b42521160630435eb91b0211d32
tknszfk <thanakornnorrasing906@gmail.com>
2024-01-15 12:16:44 +0700

    first app

--- lib/widget/list_viwe.menu.dart
+++ lib/widget/list_viwe.menu.dart
@@ -1,11 +1,25 @@
 import 'package:flutter/material.dart';
 
+class ListMenu {
+  final String? title;
+  final String? subtitle;
+  final IconData icon;
+  const ListMenu({this.title, this.subtitle,required this.icon});
+}
+
+List<ListMenu> menus = [
+  const ListMenu(title: 'Menu-1,', subtitle: 'SUB-1', icon: Icons.person),
+  const ListMenu(title: 'Menu-2,', subtitle: 'SUB-1', icon: Icons.person_add),
+  const ListMenu(title: 'Menu-3,', subtitle: 'SUB-1', icon: Icons.wifi),
+  const ListMenu(title: 'Menu-4,', subtitle: 'SUB-1', icon: Icons.build),
+];
+
 class ListViewMenu extends StatelessWidget {
   const ListViewMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return Scaffold(  
       appBar: AppBar(
         backgroundColor: Colors.orange,
         title: const Text("List View Menu"),
@@ -14,12 +28,15 @@ class ListViewMenu extends StatelessWidget {
         child: ListView.separated(
           itemBuilder: (context, index) {
             return ListTile(
-              title: const Text("MENU-1"),
-              subtitle: const Text("SUB-1"),
-              leading: const CircleAvatar(child: Icon(Icons.person)),
+              title:  Text("${menus[index].title}"),
+              subtitle:  Text("${menus[index].subtitle}"),
+              leading:  CircleAvatar(child: Icon(menus[index].icon)),
               trailing: const Icon(Icons.arrow_right),
               onTap: () {
-                debugPrint("Menu-1");
+                debugPrint("menus[index].title");
+                if(Navigator.canPop(context)){
+                  Navigator.pop(context,menus[index].title);
+                }
               },
             );
           },
@@ -28,7 +45,7 @@ class ListViewMenu extends StatelessWidget {
               color: Colors.green,
             );
           },
-          itemCount: 4,
+          itemCount: menus.length,
         ),
       ),
     );
